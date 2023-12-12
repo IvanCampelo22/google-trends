b5f0315b23d954afc76c441b8f3ba3bb3d4dc566
ivan <ivancampelo1973@gmail.com>
2023-12-11 23:06:51 -0300

    feat:arquivos fazendo upgrade e data e valor sendo resgatadosseparadamente

--- bot_trends.py
+++ bot_trends.py
@@ -1,31 +1,44 @@
+import os
 from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
-from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-from selenium.common.exceptions import TimeoutException
-from selenium.webdriver.common.keys import Keys
-from bs4 import BeautifulSoup
+from webdriver_manager.chrome import ChromeDriverManager
 from time import sleep
-import re 
-from models.graph_models import Graphic
-from database.conn import session
+from datetime import datetime
+import re
+import csv
+from selenium.webdriver.common.keys import Keys
+
+dir = '/home/ivan/Projects/Charisma/google-trends/files/'
 
 def bot_graphic(param, country: None, period: None, initial_date: None, end_date: None) -> None:
     try:
         chrome_options = Options()
-        chrome_options.add_argument("--start-maximized")  # Abre o Chrome em tela cheia
+        file_path = os.path.join(dir, 'multiTimeline.csv')
+
+        chrome_options = Options()
+        chrome_options.add_experimental_option('prefs', {
+            'download.default_directory': dir,
+            'download.prompt_for_download': False,
+            'download.directory_upgrade': True,
+            'safebrowsing.enabled': False
+        })
+
+        chrome_options.add_argument("--start-maximized")
 
         driver = webdriver.Chrome(options=chrome_options, service=Service(ChromeDriverManager().install()))
-
 
         link = 'https://trends.google.com.br/trends/'
 
         driver.get(link)
 
         sleep(3)
+
+        if os.path.exists(file_path):
+            os.remove(file_path)
 
         wait = WebDriverWait(driver, 20)
         input_element = wait.until(EC.element_to_be_clickable((By.ID, 'i7')))
@@ -100,55 +113,122 @@ def bot_graphic(param, country: None, period: None, initial_date: None, end_date
             except Exception as e:
                 print(f"Erro ao interagir com o date picker: {e}")
         
-        sleep(10)
+        sleep(20)
 
+        wait = WebDriverWait(driver, 60)
+        # Clique no botão de exportação
+        export_button = WebDriverWait(driver, 60).until(
+            EC.element_to_be_clickable((By.CSS_SELECTOR, 'button.widget-actions-item.export'))
+        )
+        export_button.click()
 
-        html = driver.page_source
-        soup = BeautifulSoup(html, 'html.parser')
-        table_div = soup.find('div', {'aria-label': 'A tabular representation of the data in the chart.'})
-        if table_div:
-            table = table_div.find('table')
-            rows = table.find_all('tr')
+        # Aguarde até que o arquivo seja baixado (ajuste o tempo conforme necessário)
+        WebDriverWait(driver, 60).until(
+            lambda x: len(os.listdir(dir)) > 0
+        )
 
-            datas = []
-            valores = []
-            datas_limpa = []
+        sleep(20)
 
+        try:
+            # Abra o arquivo CSV baixado para leitura
+            # Abra o arquivo CSV baixado para leitura
+            with open(file_path, "r", encoding="utf-8") as csv_file:
+                csv_reader = csv.reader(csv_file)
+                
+                # Ignore as primeiras linhas se não forem relevantes
+                for _ in range(6):  # Pule as 6 primeiras linhas (até "Tempo")
+                    next(csv_reader)
 
-            for row in rows[1:]:
-                cols = row.find_all('td')
-                data = cols[0].text.strip()
-                datas.append(data)
-                valor = cols[1].text.strip()
-                valores.append(valor)
-                for i in datas:
-                    date = i.split(" às ")
-                    if period == 'Na última hora' or period == 'Nas últimas 4 horas' or period == 'Ontem' or period == 'Últimos 7 dias':
-                        just_date, just_hour = date[0], date[1]
-                    else: 
-                        just_hour = None
-                    month = {"jan": "01", "fev": "02", "mar": "03", "abr": "04", "mai": "05", "jun": "06", "jul": "07", "ago": "08", "set": "09", "out": "10", "nov": "11", "dez": "12"}
-                    mon = re.findall(r'\b(\w{3})\b', data)[0].lower()
-                    mon_number = month[mon]
-                    dia = re.findall(r'\d+', data)[0]
-                    formated_date = f"{dia}-{mon_number}"
+                for row in csv_reader:
+                    # Acesse a data/hora na primeira coluna
+                    tempo = row[0]
 
-                    print("Data:", formated_date)
-                    print("Hora:", just_hour)            
-
+                    tempo_formatado = datetime.strptime(tempo, '%Y-%m-%dT%H').strftime('%Y-%m-%d %H:%M:%S')
                     
-                    data_graphic_trends = Graphic(
-                        name = param,
-                        date = formated_date,
-                        hour = just_hour,
-                        value = valor
-                    )   
+                    # Acesse o valor na segunda coluna (remova "<" se presente)
+                    valor_str = row[1].replace("<", "").strip()
+                    
+                    # Use regex para extrair apenas os números
+                    numeros = re.findall(r'\d+', valor_str)
+                    
+                    # Se houver números, pegue o primeiro número (você pode ajustar conforme necessário)
+                    valor = int(numeros[0]) if numeros else None
 
-                    session.add(data_graphic_trends)
-                    session.commit()
+                    print(f"{tempo_formatado}, {valor}")
+
+            print("Dados processados com sucesso.")
 
 
-    except TimeoutException:
-        print("Elemento não encontrado a tempo")
-        driver.close()
+        except Exception as e:
+            print(f"Erro ao processar e salvar dados no banco de dados: {e}")
+
+    finally:
+        driver.quit()
+    
+
+
+        # button = driver.find_element(By.XPATH, "//button[@title='Visualização de lista']")
+        # button.click()
+
+
+        # html = driver.page_source
+        # soup = BeautifulSoup(html, 'html.parser')
+        # table_div = soup.find('div', {'aria-label': 'A tabular representation of the data in the chart.'})
+        # if table_div:
+        #     table = table_div.find('table')
+        #     rows = table.find_all('tr')
+
+        #     datas = []
+        #     valores = []
+        #     datas_limpa = []
+
+
+        #     for row in rows[1:]:
+        #         cols = row.find_all('td')
+        #         data = cols[0].text.strip()
+        #         datas.append(data)
+        #         valor = cols[1].text.strip()
+        #         valores.append(valor)
+        #         for i in datas:
+        #             date = i.split(" às ")
+        #             if period == 'Na última hora' or period == 'Nas últimas 4 horas' or period == 'Ontem' or period == 'Últimos 7 dias':
+        #                 just_date, just_hour = date[0], date[1]
+        #             else: 
+        #                 just_hour = None
+        #             month = {"jan": "01", "fev": "02", "mar": "03", "abr": "04", "mai": "05", "jun": "06", "jul": "07", "ago": "08", "set": "09", "out": "10", "nov": "11", "dez": "12"}
+        #             mon = re.findall(r'\b(\w{3})\b', data)[0].lower()
+        #             mon_number = month[mon]
+        #             dia = re.findall(r'\d+', data)[0]
+        #             formated_date = f"{dia}-{mon_number}"
+
+        #             print("Data:", formated_date)
+        #             print("Hora:", just_hour)  
+
+        #             new_uuid = uuid.uuid4()
+
+        #             data_graphic_trends = Graphic(
+        #                     uuid = new_uuid,
+        #                     name=param,
+        #                     date=formated_date,
+        #                     hour=just_hour,
+        #                     value=valor
+        #                 )          
+                    
+        #             try:
+        #                 existing_record = session.query(Graphic).filter_by(
+        #                     name=param,
+        #                     date=formated_date,
+        #                     hour=just_hour,
+        #                     value=valor
+        #                 ).one_or_none()
+
+        #                 if existing_record:
+        #                     existing_record.uuid = new_uuid
+        #                 else:
+        #                     session.add(data_graphic_trends)
+
+        #                 session.commit()
+        #             except Exception as e:
+        #                 session.rollback()
+        #                 print(f"Erro ao inserir/atualizar registro: {str(e)}")
 
