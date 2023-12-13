60f1b2c604c8ddbfa5fcbb0688723141ee46066a
ivan <ivancampelo1973@gmail.com>
2023-12-12 23:52:26 -0300

    feat: download de arquivo funcionando

--- bot_trends.py
+++ bot_trends.py
@@ -9,8 +9,11 @@ from webdriver_manager.chrome import ChromeDriverManager
 from time import sleep
 from datetime import datetime
 import re
+import uuid
 import csv
+from models.graph_models import Graphic
 from selenium.webdriver.common.keys import Keys
+from database.conn import session
 
 dir = '/home/ivan/Projects/Charisma/google-trends/files/'
 
@@ -44,6 +47,7 @@ def bot_graphic(param, country: None, period: None, initial_date: None, end_date
         input_element = wait.until(EC.element_to_be_clickable((By.ID, 'i7')))
         input_element.clear()
         input_element.send_keys(param)
+        sleep(10)
         button = wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, 'button.UywwFc-LgbsSe')))
         button.click()
         sleep(20)
@@ -116,13 +120,11 @@ def bot_graphic(param, country: None, period: None, initial_date: None, end_date
         sleep(20)
 
         wait = WebDriverWait(driver, 60)
-        # Clique no botão de exportação
         export_button = WebDriverWait(driver, 60).until(
             EC.element_to_be_clickable((By.CSS_SELECTOR, 'button.widget-actions-item.export'))
         )
         export_button.click()
 
-        # Aguarde até que o arquivo seja baixado (ajuste o tempo conforme necessário)
         WebDriverWait(driver, 60).until(
             lambda x: len(os.listdir(dir)) > 0
         )
@@ -130,105 +132,71 @@ def bot_graphic(param, country: None, period: None, initial_date: None, end_date
         sleep(20)
 
         try:
-            # Abra o arquivo CSV baixado para leitura
-            # Abra o arquivo CSV baixado para leitura
             with open(file_path, "r", encoding="utf-8") as csv_file:
                 csv_reader = csv.reader(csv_file)
                 
-                # Ignore as primeiras linhas se não forem relevantes
-                for _ in range(6):  # Pule as 6 primeiras linhas (até "Tempo")
+                for _ in range(6): 
                     next(csv_reader)
 
                 for row in csv_reader:
-                    # Acesse a data/hora na primeira coluna
                     tempo = row[0]
 
-                    tempo_formatado = datetime.strptime(tempo, '%Y-%m-%dT%H').strftime('%Y-%m-%d %H:%M:%S')
-                    
-                    # Acesse o valor na segunda coluna (remova "<" se presente)
-                    valor_str = row[1].replace("<", "").strip()
-                    
-                    # Use regex para extrair apenas os números
-                    numeros = re.findall(r'\d+', valor_str)
-                    
-                    # Se houver números, pegue o primeiro número (você pode ajustar conforme necessário)
-                    valor = int(numeros[0]) if numeros else None
+                    try:
+                        match = re.search(r'\d{4}-\d{2}-\d{2}(?:T\d{2}:\d{2}:\d{2})?(?:[-+]\d{2}:\d{2})?', tempo)
 
-                    print(f"{tempo_formatado}, {valor}")
+                        if match:
+                            tempo_formatado = match.group(0)
 
-            print("Dados processados com sucesso.")
+                            data_hora = datetime.strptime(tempo_formatado, '%Y-%m-%dT%H:%M:%S%z') if 'T' in tempo_formatado else datetime.strptime(tempo_formatado, '%Y-%m-%d')
 
+                            data = data_hora.strftime('%Y-%m-%d')
+                            hora = data_hora.strftime('%H:%M:%S')
 
+                            valor_str = row[1].replace("<", "").strip()
+
+                            numeros = re.findall(r'\d+', valor_str)
+
+                            valor = int(numeros[0]) if numeros else None
+
+                            print(f"Data: {data}, Hora: {hora}, Valor: {valor}")
+
+                            new_uuid = uuid.uuid4()
+
+                            data_graphic_trends = Graphic(
+                                    uuid = new_uuid,
+                                    name=param,
+                                    date=data,
+                                    hour=hora,
+                                    value=str(valor)
+                                )          
+                            
+                            try:
+                                existing_record = session.query(Graphic).filter_by(
+                                    name=param,
+                                    date=data,
+                                    hour=hora,
+                                    value=str(valor)
+                                ).one_or_none()
+
+                                if existing_record:
+                                    existing_record.uuid = new_uuid
+                                else:
+                                    session.add(data_graphic_trends)
+
+                                session.commit()
+                            except Exception as e:
+                                session.rollback()
+                                print(f"Erro ao inserir/atualizar registro: {str(e)}")
+
+                    except Exception as e:
+                        print(f"Erro ao processar linha: {e}")
+
+                print("Dados processados com sucesso.")
         except Exception as e:
             print(f"Erro ao processar e salvar dados no banco de dados: {e}")
-
     finally:
         driver.quit()
     
 
 
-        # button = driver.find_element(By.XPATH, "//button[@title='Visualização de lista']")
-        # button.click()
-
-
-        # html = driver.page_source
-        # soup = BeautifulSoup(html, 'html.parser')
-        # table_div = soup.find('div', {'aria-label': 'A tabular representation of the data in the chart.'})
-        # if table_div:
-        #     table = table_div.find('table')
-        #     rows = table.find_all('tr')
-
-        #     datas = []
-        #     valores = []
-        #     datas_limpa = []
-
-
-        #     for row in rows[1:]:
-        #         cols = row.find_all('td')
-        #         data = cols[0].text.strip()
-        #         datas.append(data)
-        #         valor = cols[1].text.strip()
-        #         valores.append(valor)
-        #         for i in datas:
-        #             date = i.split(" às ")
-        #             if period == 'Na última hora' or period == 'Nas últimas 4 horas' or period == 'Ontem' or period == 'Últimos 7 dias':
-        #                 just_date, just_hour = date[0], date[1]
-        #             else: 
-        #                 just_hour = None
-        #             month = {"jan": "01", "fev": "02", "mar": "03", "abr": "04", "mai": "05", "jun": "06", "jul": "07", "ago": "08", "set": "09", "out": "10", "nov": "11", "dez": "12"}
-        #             mon = re.findall(r'\b(\w{3})\b', data)[0].lower()
-        #             mon_number = month[mon]
-        #             dia = re.findall(r'\d+', data)[0]
-        #             formated_date = f"{dia}-{mon_number}"
-
-        #             print("Data:", formated_date)
-        #             print("Hora:", just_hour)  
-
-        #             new_uuid = uuid.uuid4()
-
-        #             data_graphic_trends = Graphic(
-        #                     uuid = new_uuid,
-        #                     name=param,
-        #                     date=formated_date,
-        #                     hour=just_hour,
-        #                     value=valor
-        #                 )          
-                    
-        #             try:
-        #                 existing_record = session.query(Graphic).filter_by(
-        #                     name=param,
-        #                     date=formated_date,
-        #                     hour=just_hour,
-        #                     value=valor
-        #                 ).one_or_none()
-
-        #                 if existing_record:
-        #                     existing_record.uuid = new_uuid
-        #                 else:
-        #                     session.add(data_graphic_trends)
-
-        #                 session.commit()
-        #             except Exception as e:
-        #                 session.rollback()
-        #                 print(f"Erro ao inserir/atualizar registro: {str(e)}")
-
+        
